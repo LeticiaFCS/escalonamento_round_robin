fc8126676268eabb73d38cb46102f12b1da6cdc8
LeticiaFCS <leticiafcs@dcc.ufrj.br>
2022-05-22 16:41:37 -0300

    simulation without IO

--- filas.h
+++ filas.h
@@ -3,10 +3,10 @@
 #include<stdbool.h>
 
 #define disc 0
-#define magnetic_tape 1
+#define magneticTape 1
 #define printer 2
 
-int io_time[] = {5, 15, 20};
+int io_time[] = {4, 12, 20};
 
 typedef struct io{
 	int type;
@@ -20,25 +20,27 @@ typedef struct process{
 	
 	int arrival_time;
 	int cpu_time;
+	int used_cpu_time;
+	
 	
 	io *ios;
-	int ioQtd;
+	int io_qtd;
 	
 } process;
 
 
 // printing data of process
-void printProcess(process *p){
+void print_process(process *p){
 	if(p == NULL)
-		printf("Empty process\n");
+		printf("\tEmpty process\n\n");
 	else {
-		printf("Process with PID %d\n", p->pid);	
+		printf("\tProcess with PID %d\n\n", p->pid);	
 	}
 
 }
 
 // creating new process
-process * newProcess(int pid, int numberOfIo){
+process * new_process(int pid, int arrival_time, int cpu_time, int number_of_io){
 	//allocating memory
 	process * p = (process *) malloc (sizeof (process));
 	
@@ -48,30 +50,31 @@ process * newProcess(int pid, int numberOfIo){
 	p -> pid = pid;
 	p -> status = 0;
 	p -> priority = 0;
-	p -> arrival_time = 0;
-	p -> cpu_time = 0;
+	p -> arrival_time = arrival_time;
+	p -> cpu_time = cpu_time;
+	p -> used_cpu_time = 0;
 	
-	p->ioQtd = numberOfIo;
-	if(numberOfIo > 0)
-		p->ios = (io *) malloc(numberOfIo * sizeof(io));
+	p->io_qtd = number_of_io;
+	if(number_of_io > 0)
+		p->ios = (io *) malloc(number_of_io * sizeof(io));
 	
 		
 	return p;
 }
 
-typedef struct queueNode{
+typedef struct queue_node{
 	process *p;
-	struct queueNode *next;
-} queueNode;
+	struct queue_node *next;
+} queue_node;
 
 
 typedef struct queue{
-	queueNode *head;
-	queueNode *back;
+	queue_node *head;
+	queue_node *back;
 } queue;
 
 
-queue *initQueue(){
+queue *init_queue(){
 	queue *q = (queue *) malloc (sizeof(queue));
 	q -> head = NULL;
 	q -> back = NULL;
@@ -84,14 +87,14 @@ int empty(queue *q){
 	return 0;
 }
 
-void enqueue(queue * q, process *p){
+void push(queue * q, process *p){
 	if( empty(q) ){
-		q -> head = (queueNode *) malloc (sizeof(queueNode));
+		q -> head = (queue_node *) malloc (sizeof(queue_node));
 		q -> head -> next = NULL;
 		q -> head -> p = p;
 		q -> back = q -> head;	
 	} else {
-		q -> back -> next = (queueNode *) malloc (sizeof(queueNode));
+		q -> back -> next = (queue_node *) malloc (sizeof(queue_node));
 		q -> back -> next -> next = NULL;
 		q -> back -> next -> p = p;
 		q -> back = q -> back -> next;
@@ -117,9 +120,9 @@ void pop(queue *q){
 		q -> head = q -> back = NULL;
 	}
 	else{
-		queueNode *newHead = q -> head -> next;
+		queue_node *new_head = q -> head -> next;
 		free(q -> head);
-		q -> head = newHead;
+		q -> head = new_head;
 	}
 }
 
